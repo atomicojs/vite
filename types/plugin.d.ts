7aec5e85396c3acdcac0cefcf9bbf85ce7dc138f
UpperCod <mtrujillo@outlook.cl>
2022-04-24 20:46:27 -0400

    save

---
+++ types/plugin.d.ts
@@ -0,0 +1,10 @@
+declare function _default({ jsxImportSource, jsxPragma, jsxFactory, cssLiterals, }?: {
+    jsxImportSource?: string;
+    jsxPragma?: string;
+    jsxFactory?: string;
+    cssLiterals?: {
+        minify: boolean;
+        postcss: boolean;
+    };
+}): import("vite").Plugin;
+export default _default;
