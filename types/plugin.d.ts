756bb9b588ec35a93da1291fb5bfbc5a031a0d8c
UpperCod <uppercod.git@gmail.com>
2022-10-05 12:32:20 -0300

    types

--- types/plugin.d.ts
+++ types/plugin.d.ts
@@ -1,10 +1,10 @@
 declare function _default({ jsx, cssLiterals, tsconfigSrc, storybook, }?: {
     jsx?: boolean;
     cssLiterals?: {
-        minify: boolean;
-        postcss: boolean;
+        minify?: boolean;
+        postcss?: boolean;
     };
     tsconfigSrc?: string;
-    storybook?: boolean;
+    storybook?: string[];
 }): import("vite").Plugin[];
 export default _default;
--- types/plugins/plugin-storybook.d.ts
+++ types/plugins/plugin-storybook.d.ts
@@ -1,4 +1,5 @@
 /**
+ * @param {string[]} expression
  * @returns {import("rollup").Plugin}
  */
-export function pluginStorybook(): import("rollup").Plugin;
+export function pluginStorybook(expression: string[]): import("rollup").Plugin;
