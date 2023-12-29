a7de44a0f516bc15da55bbd47815452aa858e572
MatiasWorker <matias.trujillo@blue.cl>
2023-12-28 23:36:01 -0300

    Fix body

--- templates/cloudflare-pages.js
+++ templates/cloudflare-pages.js
@@ -4,27 +4,28 @@
  * @returns
  */
 export async function onRequestPost(context) {
-    const [, search] = context.request.url.split("?");
-    const params = Object.fromEntries(new URLSearchParams(search));
-  
-    if (!params.id || !params.use)
-      return new Response("{}", {
-        status: 404,
-        headers: {   'Content-Type': 'application/json;charset=utf-8', },
-      });
-    
+	const [, search] = context.request.url.split("?");
+	const params = Object.fromEntries(new URLSearchParams(search));
 
-    const file = atob(params.id.toString());
-  
-    const module = await import(`./_/${file}`);
+	if (!params.id || !params.use)
+		return new Response("{}", {
+			status: 404,
+			headers: { "Content-Type": "application/json;charset=utf-8" },
+		});
 
-    return new Response(
-      JSON.stringify({
-        ...await module[params.use]({})
-      }),
-      { status: 200, headers: { 
-            'Content-Type': 'application/json;charset=utf-8',
-        } }
-    );
-  }
-  
+	const file = atob(params.id.toString());
+
+	const module = await import(`./_/${file}`);
+
+	return new Response(
+		JSON.stringify({
+			...(await module[params.use](await context.request.json())),
+		}),
+		{
+			status: 200,
+			headers: {
+				"Content-Type": "application/json;charset=utf-8",
+			},
+		},
+	);
+}
