dae6635c36a3cc1c04d738f01f0ea84615cb9c40
MatiasWorker <matias.trujillo@blue.cl>
2024-03-17 23:57:24 -0300

    Fix test

--- tests/build/assets/0fcc2ab4986dd961f196e3632de8906a-Dep_ruuc.js
+++ tests/build/assets/0fcc2ab4986dd961f196e3632de8906a-Dep_ruuc.js
@@ -1,4 +1,10 @@
-import{j as t,a as e,_ as o,F as n}from"./index-naV6-d5p.js";const c=t(n,{children:[e("h2",{children:"Title 1"}),e("p",{children:"The following block allows injecting imports"}),e("h1",{children:"content 1..."}),t("pre",{children:[e("code",{class:"language-ts"}),`import { createContext } from "atomico";
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/77f90508808a050e7521384db9f9fb2f-CoxsCueZ.js","assets/index-C1SkBrZj.js","assets/index-BZtTELGf.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{j as t,a as e,_ as o,F as n}from"./index-C1SkBrZj.js";const c=t(n,{children:[e("h2",{children:"Title 1"}),e("p",{children:"The following block allows injecting imports"}),e("h1",{children:"content 1..."}),t("pre",{children:[e("code",{class:"language-ts"}),`import { createContext } from "atomico";
 import { Brand } from "./brand/brand";
 
 console.log({ Brand });
@@ -20,7 +26,7 @@ myButton.styles = css\`
 
 export const MyButton = c(myButton);
 
-customElements.define("my-button", MyButton);`]}),e("p",{children:"this content ... fail?"}),(await o(()=>import("./77f90508808a050e7521384db9f9fb2f-vgssQMUA.js"),__vite__mapDeps([0,1,2]))).default,t("pre",{children:[e("code",{class:"language-tsx"}),`import { MyTheme } from "./my-theme";
+customElements.define("my-button", MyButton);`]}),e("p",{children:"this content ... fail?"}),(await o(()=>import("./77f90508808a050e7521384db9f9fb2f-CoxsCueZ.js"),__vite__mapDeps([0,1,2]))).default,t("pre",{children:[e("code",{class:"language-tsx"}),`import { MyTheme } from "./my-theme";
 import { MyButton } from "./my-button";
 
 export default (
@@ -36,9 +42,3 @@ export default (
         </MyTheme>
     </>
 );`]}),e("h2",{children:"Title 2"}),e("p",{children:"content 4..."})]});export{c as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/77f90508808a050e7521384db9f9fb2f-vgssQMUA.js","assets/index-naV6-d5p.js","assets/index-WbUxCxn8.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
