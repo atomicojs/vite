98fd812446bda9564722f06aa2e51c4492b6b86a
MatiasWorker <matias.trujillo@blue.cl>
2024-05-18 17:13:38 -0400

    feat: Add client middleware for server actions

--- types/plugins/server-actions.d.ts
+++ types/plugins/server-actions.d.ts
@@ -2,20 +2,15 @@
  * @typedef {Object} OptionsServerActions
  * @property {string}  [src]
  * @property {TYPE_VERCEL|TYPE_CLOUDFLARE|TYPE_NETLIFY}  [type]
- * @property {{src:string,href:string}}  [options]
  * @property {string}  [folder]
  */
 /**
  * @param {OptionsServerActions} options
  * @returns {import("vite").Plugin}
  */
-export function pluginServerActions({ type, folder, src, options, }?: OptionsServerActions): import("vite").Plugin;
+export function pluginServerActions({ type, folder, src, }?: OptionsServerActions): import("vite").Plugin;
 export type OptionsServerActions = {
     src?: string;
     type?: "vercel" | "cloudflare" | "netlify";
-    options?: {
-        src: string;
-        href: string;
-    };
     folder?: string;
 };
